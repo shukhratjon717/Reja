0daada311c1c9fa6c04eadc823f8df76b130ee7d
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-05 10:44:50 +0900

    last: EJS continations

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response")
 const app = express();
 const http = require("http")
 
@@ -16,12 +17,14 @@ app.set("view engine", "ejs");
 
 
 // 4) Routing Code
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background:red"> Hello World!</h1>`)
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"})
 });
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`)
+app.get("/", function(req, res) {
+    res.render("harid")
 });
+
 
 
 const server = http.createServer(app);
