558a1067e243bf2d40ffcaa281691f062e6a5379
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-06 21:33:44 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const res = require("express/lib/response")
 const app = express();
-const http = require("http")
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err );
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 // 1) Kirish ko'dlari
 app.use(express.static("public"));
@@ -16,11 +26,17 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 
+
 // 4) Routing Code
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({test: "success"})
 });
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user} );
+});
+
 app.get("/", function(req, res) {
     res.render("harid")
 });
