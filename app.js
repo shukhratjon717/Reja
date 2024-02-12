cce418797f6f7d153840cfd6de503b9903448d12
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-12 17:29:49 +0900

    feat: frontEnd javaScriptni qurish

--- app.js
+++ app.js
@@ -34,26 +34,23 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
     console.log("user entered /created-item")
 
-    console.log(req.body);
+    // console.log(req.body);
     const new_reja = req.body.reja;
     db.collection("plans").insertOne( {reja: new_reja}, (err, data) => {
-        if (err) {
-            console.log(err);
-            console.log("Something went wrong");
-        }else {
-            res.end("Successfully added")  
-        }
+        console.log(data.ops)
+        res.json(data.ops[0]);
     });
-    // res.json({test: "success"})
 });
 
-app.get('/author', (req, res) => {
-    res.render("author", {user: user} );
-});
+// app.get('/author', (req, res) => {
+//     res.render("author", {user: user} );
+// });
 
 app.get("/", function(req, res) {
     console.log("user entered /")
-    db.collection("plans").find().toArray( (err, data) => {
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
         if (err) {
             console.log(err);
             res.end("Something went wrong")
