9e937a6b9b5b4aab6a686aaea1202050e376f98a
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-15 15:51:53 +0900

    fix: Task C completion

--- train.js
+++ train.js
@@ -1,33 +1,5 @@
 const moment = require("moment")
 
-// // Task-B
-// function findNum(num) {
-//     let count = 0;
-//     for (let i = 0; i < num.length; i++) {
-//          if(!isNaN(num[i])){
-//         count++
-//          }
-//       }
-//     return count
-//    }
-   
-//   let result = findNum('hello561')
-//   console.log(result)
-
-// // Task-A 
-
-// // let result = [];
-// // function findNum(letter, word) {
-// //   for(let i = 0; i <= word.length; i++) {
-// //     if(word[i] === letter) {
-// //        result.push(word[i])
-// //     }
-// //   }
-  
-// //   return "Task-A",result.length;
-// // }
-// // findNum("e", "engeenee")
-
 class Shop {
   // state 
   
